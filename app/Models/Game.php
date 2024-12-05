dd0a297639f8a420994e94b99a2f9333defb071f
James Randall <james@manmachineltd.com>
2024-12-06 09:31:12 +1100

    Added GameJoined Event

--- app/Models/Game.php
+++ app/Models/Game.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Events\GameJoined;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -27,6 +28,7 @@ class Game extends Model
     {
         $this->player_two_id = $player->id;
         $this->save();
+        GameJoined::dispatch($this);
         return $this;
     }
 }
